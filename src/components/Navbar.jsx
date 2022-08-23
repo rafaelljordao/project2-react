33bcd228955a9e5972c16315c4936e195042fb9d
Rafael Jordão <rafaelimajordao@gmail.com>
2022-08-23 15:53:53 -0300

    Organização e estilização do componente Navbar como barra de navegação lateral

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import './css/navbar.css'
 
 export const Navbar = () => {
     return (
-        <nav>
-            <h3>Menu de Navegação</h3>
+        <nav className="navbar">
+            <h3>Título da Navbar</h3>
             <Link to='/'>Início</Link>
             <Link to='/signup'>Cadastre-se</Link>
             <Link to='/clients'>Clientes</Link>
