b25c444e828a055c8151fd4137172b946bef112e
Rafael Jordão <rafaelimajordao@gmail.com>
2022-08-30 13:10:44 -0300

    Deleção do componente Form2 e testes com useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { Home } from './components/pages/Home/Home'
-import { SignUp } from './components/pages/SignUp/SignUp'  
+import SignUp from './components/pages/SignUp/SignUp'
 import { Clients } from './components/pages/Clients/Clients'
 import { Head } from './components/Header/Header'
 import { Navbar } from './components/Navbar/Navbar'
 import { Foot } from './components/Footer/Footer'
-import { Form2 } from './components/pages/form2/form2'
 import './app.css'
 
 import { Routes, Route } from "react-router-dom"
@@ -19,7 +18,6 @@ function App() {
           <Route path="/" element={ <Home /> } />
           <Route path="/SignUp" element={ <SignUp /> } />
           <Route path="/Clients" element={ <Clients /> } />
-          <Route path='/Form2' element={ <Form2 />} />
         </Routes>
       </div>
       <Foot />
--- src/components/pages/SignUp/SignUp.jsx
+++ src/components/pages/SignUp/SignUp.jsx
@@ -1,34 +1,36 @@
 import './signup.css'
 import { But } from '../../Button/Button'
+import { useState, useEffect } from 'react'
 
-export const SignUp = () => {
+function SignUp() {
+
+    const [className1, setClassName1] = useState("formBox1")
+    const [className2, setClassName2] = useState("formBox2")
+    const [className3, setClassName3] = useState("formBox3")
+
+    useEffect(() => {
+      setClassName1(className2)
+    }, [])
+    
 
     return (
         <div className='signUpBody'>
             <h3 className='titleForm'>CADASTRE-SE</h3>
-            <form className='formBox'>
-                {/* <label htmlFor="fname">Primeiro nome:</label> */}
+            <form className='formBox1'>
+                
+
                 <input type="text" className='inputBox' name="fname" placeholder="Digite seu primeiro nome..." />
 
-                {/* <label htmlFor="lname">Sobrenome:</label> */}
                 <input type="text" className='inputBox' name="lname" placeholder="Digite seu sobrenome..." />
 
-                {/* <label htmlFor="email">E-mail:</label> */}
                 <input type="text" className='inputBox' name="email" placeholder="Digite seu E-mail..." />
 
-                {/* <label htmlFor="cellphone">Telefone:</label> */}
                 <input type="text" className='inputBox' name="cellphone" placeholder="Digite seu telefone..." />
 
-                {/* <div className='radioBox'>
-                    <input type="radio" name="gender" className='inputRadio' />
-                    <label htmlFor="gender" className='lblRadio'>Masculino</label>
-
-                    <input type="radio" name="gender" className='inputRadio' />
-                    <label htmlFor="gender">Feminino</label>
-                </div> */}
-
-                <But txt="Próximo" />
-            </form>      
+                <But txt="Próximo" onClick={() => setClassName1(className2)} />
+            </form>
         </div>
     )
 }
+
+export default SignUp;
